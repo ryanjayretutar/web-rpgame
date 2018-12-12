0588670f9db15e60d410c4e37a4493f016bb190d
ryanjayretutar <pahiram.retutar@gmail.com>
2018-12-13 00:54:22 +0800

    First Push Project 2

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 500) {
+        $(".navbar").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".navbar").removeClass("active");
+    }
+});
